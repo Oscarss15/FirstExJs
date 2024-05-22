cf664104c86054c5ad2f1307a8077aa382e9b669
Oscar <oscar15-91@hotmail.com>
2024-05-22 20:20:40 +0200

    Añadido ejercicio4

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ import { name } from "./js/ex1.js";
 import { printTerminal, suma } from "./js/ex2.js";
 import { stringLowerCase } from "./js/ex3.js";
 import { word } from "./js/ex3.js";
-
 import { wordincludes } from "./js/ex3.js";
-
+import { invertirString } from "./js/ex4.js";
+import { cadenaString } from "./js/ex4.js";
 let num1=3;
 let num2=5;
-
 
 
 
@@ -25,7 +24,9 @@ function app(){
    
 printTerminal(name);
 printTerminal(suma(num1,num2));
-console.log(wordincludes());
+console.log(invertirString(cadenaString));
+
+
 
 }
 
@@ -35,7 +36,6 @@ function sumar(param,param2){
     console.log(param+param2);
 return param+param2;
 }
-
 
 
 
